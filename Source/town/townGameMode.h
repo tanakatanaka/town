d02bdf032a2937f5e7b36acc6940093954adaba5
tanakatanaka <aiuro2003haru@gmail.com>
2022-04-14 00:01:54 +0900

    asset追加1

---
+++ Source/town/townGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "townGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AtownGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AtownGameMode();
+};
+
+
+
